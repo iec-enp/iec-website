8fbeda66e2970001e7c38100ee87ac2670d244cc
Raihaen <100054176+Raihaen@users.noreply.github.com>
2024-02-21 17:59:41 +0100

    Updated events data

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -20,10 +20,10 @@ export default function Events() {
         <EventSection
           name='Business Game'
           description="Business game est un jeu de simulation de gestion consistant en une compétition entre des entreprises virtuelles en concurrence sur le marché. Chaque entreprise est représentée et gérée par une équipe de 4 à 5 participants. Le jeu permet de développer de manière ludique et interactive des connaissances liées à l'analyse financière et à l'entrepreneuriat, et de mettre en avant leurs compétences managériales."
-          date='12-13 Mai 2022'
-          place='Alger'
+          date='04-05 Mai 2023'
+          place='HIS - Higher Institue of Science, Bordj El Kiffan'
           pics={['/images/pictures/img2.webp']}
-          edition='2'
+          edition='3'
           workshop='20'
           participant='50'
           logo='/brand/logos/bg-logo.png'
@@ -49,10 +49,10 @@ export default function Events() {
           logo='/images/events/logo-icc.webp'
           name='IEC Case Compétition '
           description="ICC est une compétition d'étude de cas organisée par le club IEC dans laquelle les participants doivent tenter de trouver une solution à une problématique d'actualité proposée par une entreprise."
-          date='13-14- 15-16 Octobre 2021'
-          place='Hotel Tulip Inn Naya - Bab Ezzouar'
+          date='17-19 Novembre 2022'
+          place='Salle Dar El Djazair - SAFEX'
           pics={['/images/pictures/img4.webp']}
-          edition='2'
+          edition='3'
           participant='48'
           color={['color-icc-dark', 'color-icc']}
           iconColor={['#da8ee4', '#421B38']}
@@ -62,11 +62,11 @@ export default function Events() {
           logo='/images/events/ADC_logo.svg'
           name='Algeria Data CUP'
           description="Algeria Data Cup est un hackathon de data science où les participants sont invités à résoudre un problème de Business réel, en développant un modèle d'apprentissage automatique le plus performant possible sur les données fournies. La 1ère édition s'est déroulée sur la plateforme Zindi.com."
-          date='16-18 Décembre 2021'
-          place='En ligne sur la plateforme ZINDI '
+          date='17-19 Juin 2023'
+          place='HIS - Higher Institute of Sciences, Borj El Kiffan '
           pics={['/images/pictures/adc.webp']}
-          edition='1'
-          participant='100'
+          edition='2'
+          participant='64'
           left={false}
           color={['color-adc-dark', 'color-adc']}
           iconColor={['#7eaafa', '#4A8AFC']}
@@ -76,10 +76,10 @@ export default function Events() {
           logo='/images/events/jourj_logo.webp'
           name='Jour Génie Industriel'
           description="Le Jour GI est un événement qui se déroule chaque année, pour but de promouvoir la spécialité du Génie Industriel sous ses deux options aux étudiants lauréats du concours national d'accès aux grandes écoles d'ingénieurs"
-          date='Début Septembre'
+          date='Fin Juin'
           place='En ligne sur les pages réseaux de IEC'
           pics={['/images/pictures/gi.webp']}
-          edition='2'
+          edition='3'
           color={['color-ji-dark', 'color-ji']}
           iconColor={['#f3ae2f', '#D88F0A']}
         />
